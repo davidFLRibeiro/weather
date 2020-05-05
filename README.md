57c2e68444bd6b058ac5c44158ac8533a9ad9127
David Ribeiro <davidflribeiro87@gmail.com>
2020-05-05 20:59:18 +0000

    first commit

---
+++ README.md
@@ -0,0 +1,18 @@
+Azores Islands Beach Weather Tracker
+
+link: https://repl.it/@davidzribeiro/Azores-Islands-Beach-Weather-Tracker
+
+screenshots:
+Home: https://imgur.com/a/6s6kj4c
+In the home page we have a list of the Islands of the Azores, clicking on th island we will go to island info.
+
+Island Info: https://imgur.com/a/tixeXto
+On the Island info we can see the temperature humidity and percipitation,
+and the app will try to help you if it´s a good day to go to the beach.
+
+The app is about weather and news from azores that are allways good,
+we can see if weather it´s good or not to go to the beach. and see some news on the beach.
+
+Technology used: HTML, CSS ,JavaScript, JQuery
+
+The API that it´s free and it´s from weatherapi.com
---
+++ islandData.js
@@ -0,0 +1,46 @@
+const islandData = {
+  'Santa Maria': {
+    handle: 'santa%20maria%20vila%20do%20porto%20azores',
+    img:
+      'https://www.azoreschoice.com/wp-content/uploads/sites/8/2016/11/Santa_Maria_Azores_Holidays.praia_Formosa_beach.Santa_Maria._Large_-1.jpg',
+  },
+  'Ponta Delgada': {
+    handle: 'Ponta%20Delgada',
+    img:
+      'https://www.planetware.com/photos-large/P/portugal-ponta-delgada-lagoa-das-sete-cidades-sao-miguel-azores.jpg',
+  },
+  Terceira: {
+    handle: 'angra%20do%20heroismo',
+    img: 'https://i.ytimg.com/vi/rAEyYHI_t-k/maxresdefault.jpg',
+  },
+  Faial: {
+    handle: 'horta%20azores',
+    img:
+      'https://media-cdn.tripadvisor.com/media/photo-s/07/81/f0/df/sailazores-yacht-charter.jpg',
+  },
+  Pico: {
+    handle: 'sao%20roque%20do%20pico',
+    img:
+      'https://www.geekyexplorer.com/wp-content/uploads/2017/08/experiences-things-to-do-in-pico-atlantic-swim-pool.jpg',
+  },
+  Graciosa: {
+    handle: 'santa%20cruz%20da%20graciosa',
+    img:
+      'https://azoresway.files.wordpress.com/2017/01/graciosa-island-scenic-view2.jpg?w=800&h=500&crop=1',
+  },
+  Corvo: {
+    handle: 'corvo%20azores',
+    img:
+      'https://travelanubhav.com/wp-content/uploads/2018/05/Corvo-Azores-TravelAnubhav-1170x550.jpg',
+  },
+  Flores: {
+    handle: 'flores%20azores',
+    img:
+      'https://azo-cdn.azureedge.net/sites/default/files/inline-images/ilha-das-flores.jpg',
+  },
+  'São Jorge': {
+    handle: 'sao%20jorge%20azores',
+    img:
+      'https://www.azoreschoice.com/wp-content/uploads/sites/8/2016/11/Azores_Holidays.Faja_walk.Sao_Jorge._Large_.jpg',
+  },
+};
